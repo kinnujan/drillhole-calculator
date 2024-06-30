a2fa61d317cd800b78f8e692b951eb3fcf1ace6f
kinnujan <55635375+kinnujan@users.noreply.github.com>
2024-06-30 11:00:32 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,14 +12,17 @@ const urlsToCache = [
   '/favicon.ico',
   '/apple-touch-icon.png',
   '/favicon-32x32.png',
-  '/favicon-16x16.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/mathjs/11.8.0/math.js'
+  '/favicon-16x16.png'
 ];
 
 self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
@@ -48,16 +51,24 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
       );
     })
   );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
